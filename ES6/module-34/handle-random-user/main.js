446acfd951f97e26e9078f43ac2493a623f6c8b8
asru-islam <arasru01@gmail.com>
2023-02-26 11:23:11 +0600

    update practice file

--- ES6/module-34/handle-random-user/main.js
+++ ES6/module-34/handle-random-user/main.js
@@ -10,16 +10,28 @@ const loadUser = () => {
 
 const displayUser = (user) => {
   // console.log(user);
-  //   console.log(user.results[0].name);
+  console.log(user.results[0].picture.large);
 
   const gender = document.getElementById("gender");
   gender.innerText = user.results[0].gender;
 
-  const name = document.getElementById("name");
   const fullName = user.results[0].name;
+  const name = document.getElementById("name");
   name.innerHTML = `
         ${fullName.title} ${fullName.first} ${fullName.last}
     `;
+
+  const address = user.results[0].location;
+  const location = document.getElementById("location");
+  location.innerHTML = `
+        ${address.street.number} ${address.street.name}, ${address.city}, ${address.state} ${address.postcode}, ${address.country}
+    `;
+
+  const setImg = user.results[0].picture.large;
+  const img = document.getElementById("img");
+  const src = document.createAttribute("src");
+  src.value = setImg;
+  img.setAttributeNode(src);
 };
 
 loadUser();
