637b4626e7786e2274e65f549e6a300dc802bff2
asru-islam <arasru01@gmail.com>
2023-02-27 08:09:04 +0600

    update files

---
+++ ES6/module-35/main.js
@@ -0,0 +1,38 @@
+/** 
+ * ! Fetch
+ * 1. fetch must provide url
+ * dynamic or static
+ * 2. how to convert fetch promise return or json
+ * 3. how to convert json to data
+ * 4. [cool headed]:
+ * user:
+ * -> array of object
+ * -> object with property users
+ * -> object with a property called data
+ * 
+ * Nested Object
+ * 
+ * 
+ * ----------------------------------------
+ * DOM Manipulation
+ * 1. get something from the DOM
+ * 2. create element to append to the dom
+ * 3. dynamically load data based on id
+ * 
+ * 
+ * -------------------------------
+ * array -> forEach, map, find, filter
+ * 
+ * template String
+*/
+
+
+
+
+console.log(1);
+
+setTimeout(() => {
+    console.log(2);
+}, 3000);
+console.log(3);
+
