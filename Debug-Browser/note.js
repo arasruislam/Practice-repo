8e3dac2cd1665631fff4680edc52a55784d2d527
asru-islam <arasru01@gmail.com>
2023-03-10 14:06:07 +0600

    update list

---
+++ Debug-Browser/note.js
@@ -0,0 +1,37 @@
+/* 
+? setInterval(function, millisecond)
+* The setInterval() method, offered on the Window and Worker interfaces, repeatedly calls a function or executes a code snippet, with a fixed time delay between each call.
+
+? clearInterval(intervalID)
+* The global clearInterval() method cancels a timed, repeating action which was previously established by a call to setInterval(). If the parameter provided does not identify a previously established action, this method does nothing.
+*/
+
+
+/* 
+? setTimeout(function, millisecond)
+* The global setTimeout() method sets a timer which executes a function or specified piece of code once the timer expires.
+
+? clearTimeout(timeoutID)
+* The global clearTimeout() method cancels a timeout previously established by calling setTimeout(). If the parameter provided does not identify a previously established action, this method does nothing.
+*/
+
+
+/* 
+? try, catch, finally, throw
+try {
+    code ..
+    throw 'error'
+}catch {
+    code ...
+}finally {
+    code ...
+}
+
+*/
+
+
+/* 
+?
+*
+
+*/
