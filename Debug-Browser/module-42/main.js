28190a4e1e49fab9278916762512e5445ed83d85
asru-islam <arasru01@gmail.com>
2023-03-15 08:19:57 +0600

    update file

---
+++ Debug-Browser/module-42/main.js
@@ -0,0 +1,43 @@
+/* 
+১. ব্রাউজার কিভাবে কাজ করে। কিভাবে DOM tree, Render Tree বানায় সেটা ছোট করে হলেও নামগুলো জানতে হবে
+
+২. alert, confirm, prompt এই গুলা কোনটা দিয়ে কী কাজ করে। এবং এইগুলার মধ্যে পার্থক্য কি?
+
+৩. URL এর মধ্যে মেজর যে যে অংশ থাকে যেমন query string, href, hash, port এইগুলা জানতে হবে। 
+
+৪. location এর মধ্যে href দিয়ে যে যে ওয়েবসাইট দেখতেছো সেটা চেইঞ্জ করে ফেলা যায় সেটা জানতে হবে। 
+
+৫. History api এর মধ্যে যে go দিয়ে সামনে পিছনে যেতে পারো সেটা একটু খেয়াল করতে হবে। 
+
+৬. Cookies কি জিনিস। এইটা দিয়ে কি করা হয়। সেটা সম্পর্কে হালকা করে জানতে হবে। 
+
+৭. local storage আর session storage কখন কোনটা ইউজ করতে হয় সেটা জানতে হবে 
+
+৮. Cookies, local storage, session storage এর মধ্যে পার্থক্য কি সেটা জানতে হবে। 
+*/
+
+/* 
+? Browser Api
+
+1. alert()
+* The alert() method displays an alert box with a message and an OK button.
+
+2. confirm()
+* The confirm() method displays a dialog box with a message, an OK button, and a Cancel button. The confirm() method returns true if the user clicked "OK", otherwise false .
+
+3. prompt()
+* Instructs the browser to display a dialog with an optional message prompting the user to input some text, and to wait until the user either submits the text or cancels the dialog
+
+4. browser location API
+=> method[assign(), replace(), reload(), toString(), .href]
+
+5. Browser history API
+=> method [back(), forward(), go(), .length]
+
+6. Browser Cookie
+
+7. Browser localStorage -- save everything as a string value
+=> method [getItem('key'), setItem('key', 'value'), removeItem('key'), clear()]
+=> object save to localStorage--- JSON.stringify(object)
+
+*/
